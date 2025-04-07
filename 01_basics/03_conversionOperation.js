069db0e3001d510cbc2f860c0f5d1b41f691df9e
Mohd Zaid <mohdzaid4659@gmail.com>
2025-04-07 04:01:03 +0000

    Conversion and DataType

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let scroe = "true"
+
+console.log(typeof scroe)
+console.log(typeof (scroe))
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let valueInNumber = Number(scroe)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let isLoggedIn = true
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+
+let someNumber = 123
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
